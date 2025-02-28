fe71d3202b2733323bb8543a181023cfef87f07f
Zachary Lentz <zlentz@slac.stanford.edu>
2025-02-28 15:31:16 -0800

    Automatic backup @ Fri Feb 28 15:31:16 PST 2025

---
+++ screens/TMO/VAT590/pydm-tmo-mrco-vat590.cmd
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# Setup the common directory env variables
+if [ -e      /reg/g/pcds/pyps/config/common_dirs.sh ]; then
+	source   /reg/g/pcds/pyps/config/common_dirs.sh
+elif [ -e    /afs/slac/g/pcds/pyps/config/common_dirs.sh ]; then
+	source   /afs/slac/g/pcds/pyps/config/common_dirs.sh
+fi
+
+# Use pcds-conda env 
+source ${ENG_TOOLS_SCRIPTS}/pcds_conda
+
+#export IOC_PV=IOC:TMO:TST
+export BASE=MRCO:VCN:01
+
+pushd /reg/g/pcds/epics-dev/screens/pydm/vacuumscreens/screens/TMO/VAT590
+pydm -m "DEV=${BASE}" vat590.ui &
